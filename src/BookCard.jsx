9aa46dccd0e3b3268b4859eb25d1fb5114190921
Samuel Eliot Nathanson <sen52@drexel.edu>
2020-10-03 11:43:08 -0400

    Transformed React app into Chrome Extension

--- src/BookCard.jsx
+++ src/BookCard.jsx
@@ -15,7 +15,7 @@ const BookCard = ({
   // States
   const [modal, setModal] = useState(false);
   const toggle = () => setModal(!modal);
-
+  console.log(thumbnail)
   return (
     <Card style={{ width: '233px' }} className='m-auto '>
       <CardImg
